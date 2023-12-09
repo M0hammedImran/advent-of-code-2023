05a924faf2f3e52f249632ab90e7a4da7e33e1ea
Mohammed Imran <mohammedimran86992@gmail.com>
2023-12-09 21:23:38 +0530

    Add Solution for 1st half

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = match fs::read_to_string("./puzzle-input.txt") {
+        Ok(data) => data,
+        Err(err) => panic!("{}", err),
+    };
+
+    let val = contents
+        .split('\n')
+        .map(|line| {
+            return line
+                .split("")
+                .filter_map(|number_or_str| number_or_str.parse::<i32>().ok())
+                .collect::<Vec<_>>();
+        })
+        .map(|numbers| {
+            return (numbers.first().unwrap().to_string() + &numbers.last().unwrap().to_string())
+                .parse::<i32>()
+                .unwrap();
+        })
+        .sum::<i32>();
+
+    println!("{:?}", val);
 }
