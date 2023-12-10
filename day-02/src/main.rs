04848c15266a735b426e993fc7c6c5be99665c21
Mohammed Imran <mohammedimran86992@gmail.com>
2023-12-10 19:47:33 +0530

    Add day-02 code and input files

---
+++ day-02/src/main.rs
@@ -0,0 +1,41 @@
+use std::fs;
+
+fn main() {
+    let contents = match fs::read_to_string("./puzzle-input.txt") {
+        Ok(data) => data,
+        Err(err) => panic!("{}", err),
+    };
+
+    println!("part one: {}", part_one(contents.clone()));
+}
+
+fn part_one(input_string: String) -> u32 {
+    let mut sum = 0;
+
+    'game: for line in input_string.lines() {
+        let (game, sets) = line.split_once(": ").unwrap();
+        let (_, game_id) = game.split_once(' ').unwrap();
+
+        for set in sets.split("; ") {
+            for cube in set.split(", ") {
+                let (count, color) = cube.split_once(' ').unwrap();
+                let count = count.parse::<u32>().unwrap();
+                let is_valid = match color {
+                    "red" => count <= 12,
+                    "green" => count <= 13,
+                    "blue" => count <= 14,
+                    _ => true,
+                };
+
+                if !is_valid {
+                    continue 'game;
+                }
+            }
+        }
+
+        sum += game_id.parse::<u32>().unwrap();
+        println!("game: {}", game_id)
+    }
+
+    sum
+}
