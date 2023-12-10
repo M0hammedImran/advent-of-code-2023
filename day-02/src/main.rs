18ca85aa3ce944f2982c1a29d3479502400db691
Mohammed Imran <mohammedimran86992@gmail.com>
2023-12-10 20:09:47 +0530

    Add part_two of solution

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let contents = match fs::read_to_string("./puzzle-input.txt") {
@@ -7,6 +7,7 @@ fn main() {
     };
 
     println!("part one: {}", part_one(contents.clone()));
+    println!("part_two: {}", part_two(contents.clone()));
 }
 
 fn part_one(input_string: String) -> u32 {
@@ -35,6 +36,41 @@ fn part_one(input_string: String) -> u32 {
 
         sum += game_id.parse::<u32>().unwrap();
         println!("game: {}", game_id)
+    }
+
+    sum
+}
+
+fn part_two(input_string: String) -> u32 {
+    let mut sum = 0;
+
+    for line in input_string.lines() {
+        let (_, sets) = line.split_once(": ").unwrap();
+
+        let mut draws: HashMap<&str, u32> = HashMap::new();
+        for set in sets.split("; ") {
+            for cube in set.split(", ") {
+                let (count, color) = cube.split_once(' ').unwrap();
+                let count = count.parse::<u32>().unwrap();
+                let val = draws.get(color);
+
+                if val.is_none() {
+                    draws.insert(color, count);
+                }
+
+                if count > *draws.get(color).unwrap() {
+                    draws.insert(color, count);
+                }
+            }
+        }
+        let mut sub_total = 1;
+
+        for (_, draw) in draws.iter() {
+            sub_total *= draw;
+        }
+
+        sum += sub_total;
+        // println!("sum: {:?}", sub_total)
     }
 
     sum
